889963e5c0a6bf148c423d443a93cd5da775ee0e
Ag Ibragimov <agzam.ibragimov@gmail.com>
2024-02-06 21:31:17 -0600

    added plain-text version

--- src/resume/content.clj
+++ src/resume/content.clj
@@ -3,7 +3,10 @@
             [clojure.java.io :as io]
             [resume.page :as page]
             [resume.styles :as styles]
-            [integrant.core :as ig]))
+            [integrant.core :as ig]
+            [hiccup.core :as h]
+            [clojure.string :as str])
+  (:import [org.jsoup Jsoup]))
 
 (defn get-data []
   (->> "fullstack-dev.edn"
@@ -11,11 +14,70 @@
        edn/read-string
        eval))
 
+(defn hiccup->text [hiccup]
+  (cond
+    (string? hiccup) hiccup
+    (sequential? hiccup)
+    (let [tag  (first hiccup)
+          rest (next hiccup)]
+      (case tag
+        :p  (str (str/join " " (map hiccup->text rest)) "\n")
+        :ul (str/join "" (map #(str "- " (hiccup->text %) "\n") rest))
+        (str/join " " (map hiccup->text rest))))
+    :else ""))
+
+(defn generate-plain-text
+  "Generate plain text resume, for automated resume parsing systems."
+  [data]
+  (let [personal-info (format
+                       (str
+                        "%s\n%s\n%s\n"
+                        "plain-text version for automated parsers, "
+                        "for a human-readable, visit:\n"
+                        "https://agzam.github.io/resume\n\n")
+                       (-> data :basics :name)
+                       (-> data :basics :label)
+                       (-> data :basics :email))
+        summary (->> data :basics :summary
+                    :content hiccup->text
+                    (format "# Summary:\n%s\n"))
+        parse-exp (fn [{:keys [highlights
+                               start end
+                               company position
+                               keywords]}]
+                    (format (str "Company: %s\n"
+                                 "Title: %s\n"
+                                 "From: %s\n"
+                                 (if end
+                                  "To: %s\n"
+                                  "%s")
+                                 "Keywords: %s\n"
+                                 "Description:\n%s\n")
+                            company
+                            position
+                            start
+                            (or end "")
+                            (->>
+                             keywords
+                             (map name)
+                             (str/join ", "))
+                            (hiccup->text highlights)))
+        experience (->> data :work :content
+                        (map parse-exp)
+                        (str/join ""))]
+    (str personal-info
+         summary
+         "# Work experience:\n\n"
+         experience)))
+
 (defn generate []
-  (->> (get-data)
-       page/index
-       (spit "docs/index.html"))
-  (styles/generate "docs/styles.css"))
+  (let [data (get-data)]
+    (->> data generate-plain-text
+         (spit "docs/resume.txt"))
+   (->> data
+        page/index
+        (spit "docs/index.html"))
+   (styles/generate "docs/styles.css")))
 
 (defmethod ig/init-key ::generate [_ _]
   (generate))
